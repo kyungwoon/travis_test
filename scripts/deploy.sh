a83e3a244ed473e3d736665f63acba2965075eec
kyungwoon <nakw0410@gmail.com>
2022-03-23 17:05:21 +0900

    travis 자동배포 테스트4

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 REPOSITORY=/home/ec2-user/app/step2
-PROJECT_NAME=hanghae8-admin
+PROJECT_NAME=trvis_test
 
 echo "> Build 파일 복사"
 
@@ -9,7 +9,7 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl hanghae8-admin| grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl trvis_test| grep jar | awk '{print $1}')
 
 echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
 
@@ -34,9 +34,9 @@ chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 
 nohup java -jar \
-#    -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
-#    -Dspring.profiles.active=real \
-#    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
-     -Dspring.config.location=classpath:/application.properties \
-     -Dspring.profiles.active=real \
-     $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+    -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+    -Dspring.profiles.active=real \
+    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+#     -Dspring.config.location=classpath:/application.properties \
+#     -Dspring.profiles.active=real \
+#     $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
