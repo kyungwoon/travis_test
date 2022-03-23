225a23503d1348ff727040c34d73959315c377fc
kyungwoon <nakw0410@gmail.com>
2022-03-23 17:49:32 +0900

    travis 자동배포 테스트_deploy저장위치 변경

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 REPOSITORY=/home/ec2-user/app/step2
-PROJECT_NAME=travis_test
+PROJECT_NAME=hanghae8-admin
 
 echo "> Build 파일 복사"
 
-cp $REPOSITORY/zip/build/libs*.jar $REPOSITORY/
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl travis_test| grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl hanghae8-admin| grep jar | awk '{print $1}')
 
 echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
 
